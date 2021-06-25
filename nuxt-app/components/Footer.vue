6b34783d1094c12a8fd834748046be6751e8406f
Ryo Matsunami <jane.doe3wtp4649@gmail.com>
2021-06-26 07:17:07 +0900

    components add

---
+++ nuxt-app/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <v-footer :fixed="true" app>
+      <span>&copy; {{ new Date().getFullYear() }} R.M</span>
+    </v-footer>
+  </div>
+</template>
---
+++ nuxt-app/components/Navigator.vue
@@ -0,0 +1,78 @@
+<template>
+  <div>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn icon @click.stop="miniVariant = !miniVariant">
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-btn icon @click.stop="clipped = !clipped">
+        <v-icon>mdi-application</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+        {
+          icon: 'mdi-grid',
+          title: 'Grid',
+          to: '/Grid',
+        },
+        {
+          icon: 'mdi-alert-circle',
+          title: 'Alert',
+          to: '/Alert',
+        },
+        {
+          icon: 'mdi-account-circle',
+          title: 'avatar',
+          to: '/Avatar',
+        },
+      ],
+      miniVariant: false,
+      title: 'Vuetify.js',
+    }
+  },
+}
+</script>
