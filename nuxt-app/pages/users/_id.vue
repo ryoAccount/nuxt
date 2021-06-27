74786c240dd7f6dacfb6b4285c516f94d94f6ee4
Ryo Matsunami <jane.doe3wtp4649@gmail.com>
2021-06-28 05:45:24 +0900

    router

---
+++ nuxt-app/pages/users/_id.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>ユーザID: {{ $route.params.id }}</h1>
+    <a href="/users/list">back</a>
+  </div>
+</template>
---
+++ nuxt-app/pages/users/list.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>User List</h1>
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user.id }} : <a :href="user.id">{{ user.name }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      users: [
+        { id: 1, name: 'Adam' },
+        { id: 2, name: 'Baba' },
+        { id: 3, name: 'Camel' },
+      ],
+    }
+  },
+}
+</script>
